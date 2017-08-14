be68b63d6e3e637bb8cd62d8928397d80db9451e
judge2020 <judge2020@judge2020.com>
2017-08-13 21:45:07 -0400

    Dynamically warn if ppython.exe isn't defined.

--- start-otp-dev.bat
+++ start-otp-dev.bat
@@ -2,8 +2,16 @@
 
 color c
 
+REM this is taking the contents of PPYTHON_PATH.txt and putting it in an Environment varaible.
+set /P PPYTHON_PATH=<PPYTHON_PATH.txt
+
 title OpenOTP
 :goto
-C:\Panda3D-1.8.1\python\ppython __main__.py
+if exist %PPYTHON_PATH% (
+%PPYTHON_PATH% __main__.py
+) else (
+echo You seem to be missing %PPYTHON_PATH%, which is required to run OpenOTP.
+echo Please install Panda3D-1.8.1 or edit PPYTHON_PATH.txt to where your ppython is.
+)
 pause
 goto :goto
